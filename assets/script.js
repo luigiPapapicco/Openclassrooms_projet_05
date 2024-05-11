074a967b1b8830bb73dbfb5464a8a0ff50d7be4b
Luigi Papapicco <luigi.papapicco.pro@gmail.com>
2024-05-11 18:36:34 +0200

    mise en place des event Listeners sur les fleches

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,17 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+//  variable récupérant ma liste de boutons
+let listBtns = document.querySelectorAll(".arrow");
+// console.log(listBtns);
+// boucle avec un addEventlistener opérant sur chaque bouton
+
+for (let i=0; i < listBtns.length; i++) {
+	let btnActuel = listBtns[i];
+
+	btnActuel.addEventListener("click", (event) => {
+		// verifie si je clique sur la class arrow_lef, si c'est le cas alors classDiv prend la valeur fleche gauche sinon elle prend la valeur fleche droite.
+		let classDiv = event.currentTarget.classList.contains("arrow_left") ? "fleche_gauche" : "fleche_droite";
+		console.log(`J'ai cliqué sur la classe : ${classDiv}`)
+	})
+} 
