e89a38ff3a43b27aa1fb0516e226c3e2a96c9e99
Luigi Papapicco <luigi.papapicco.pro@gmail.com>
2024-05-12 17:57:35 +0200

    mise en place des bulletts, de la fonction pour la maj des img, des fonctionnalitees pour l_activation des fleches

--- assets/script.js
+++ assets/script.js
@@ -16,17 +16,75 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
-//  variable récupérant ma liste de boutons
-let listBtns = document.querySelectorAll(".arrow");
-// console.log(listBtns);
-// boucle avec un addEventlistener opérant sur chaque bouton
 
-for (let i=0; i < listBtns.length; i++) {
-	let btnActuel = listBtns[i];
+// Ajout des eventListener sur les flèches
+const left_btn = document.querySelector(".arrow_left");
+const leftClicked = left_btn.addEventListener("click", (event) => {
+	console.log("J'ai cliqué sur ma fleche gauche");
+})
+const right_btn = document.querySelector(".arrow_right");
+const rightClicked = right_btn.addEventListener("click", (event) => {
+	console.log("J'ai cliqué sur ma fleche droite");
+})
 
-	btnActuel.addEventListener("click", (event) => {
-		// verifie si je clique sur la class arrow_lef, si c'est le cas alors classDiv prend la valeur fleche gauche sinon elle prend la valeur fleche droite.
-		let classDiv = event.currentTarget.classList.contains("arrow_left") ? "fleche_gauche" : "fleche_droite";
-		console.log(`J'ai cliqué sur la classe : ${classDiv}`)
-	})
-} 
+
+// récupération du la class parent qui acceuillera mes bulletts
+const divDots = document.querySelector(".dots");
+// Ajouter des points (buletts) pour chaque slide
+for (let i = 0; i < slides.length; i++) {
+    const divDot = document.createElement("div");
+    divDot.classList.add("dot");
+	if (i === 0) {
+		divDot.classList.add("dot_selected"); // ma première iterration ajoute également la classe "dot_selected"
+	}
+    divDots.appendChild(divDot);
+}
+
+// Récupérer toutes les classe ".dot" (buletts)
+const buletts = document.querySelectorAll(".dot");
+
+
+// Fonction pour mettre à jour le chemin d'image
+function updateImagePath(imageName) {
+	return "./assets/images/slideshow/" + imageName; // concaténation du chemin au lien présent dans le tableau
+}
+
+// Fonction pour mettre à jour l'affichage selon l'index du slide
+function updateSlide(index) {
+	const slide = slides[index]; // récup de l'index pour link l'image du tableau "slide"
+    const slideImage = document.querySelector(".banner-img"); // recup du parent pour la maj de l <img>
+    const slideTagLine = document.querySelector("#banner > p"); // redup du parent pour la maj du <p>
+
+    slideImage.src = updateImagePath(slide.image); // maj de la source avec l'appel de la fonction updateImagePath
+    slideTagLine.innerHTML = slide.tagLine; // maj du paragraphe avec la parti tagLine du dictionnaire
+
+    // Mettre à jour le pointeur actif
+    buletts.forEach((dot, i) => { //pour chaque iterration sur la collectionn de bullet
+        if (i === index) { // si est strictement égale a l'index en cours
+            dot.classList.add("dot_selected"); // alors on ajoute la classe "dot_selected"
+        } else {
+            dot.classList.remove("dot_selected"); // sinon on la supprime
+        }
+    });
+}
+
+// Index du slide actuellement affiché
+let currentSlideIndex = 0;
+
+
+right_btn.addEventListener("click", () => { // Au clic sur la flèche droite
+    currentSlideIndex++; // indexEncour incrémenter de 1
+    if (currentSlideIndex >= slides.length) { //si indexEncour est supérieur ou égale a la longueur du dictionnaire
+        currentSlideIndex = 0; //alors indexEncour repasse a zero
+    }
+    updateSlide(currentSlideIndex); //on appelle la fonction avec pour paramètre la valeur de l'index
+});
+
+// Au clic sur la flèche gauche, passer au slide suivant
+left_btn.addEventListener("click", () => {
+    currentSlideIndex--; // indexEncour décrementer de 1
+    if (currentSlideIndex < 0) { // si indexEncour est inferieur a 0 
+        currentSlideIndex = slides.length - 1; // alor indexEncour egal a la longueur du dictionnaire - 1
+    }
+    updateSlide(currentSlideIndex);
+});
